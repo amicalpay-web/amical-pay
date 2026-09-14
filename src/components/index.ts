6ed00b92003c5ab31853f5856d529e3aac05bdab
Amical Pay <amicalpay@gmail.com>
2026-09-14 09:53:12 -0400

    feat(header): export reusable header components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,6 @@ export { Input } from './Input';
 export { Select } from './Select';
 export { Alert } from './Alert';
 export { LoadingSpinner } from './LoadingSpinner';
+export { default as Header } from './Header'
+export { MobileMenu } from './MobileMenu'
+export { UserMenu } from './UserMenu'
