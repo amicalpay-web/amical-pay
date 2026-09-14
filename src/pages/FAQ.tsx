77de50ed87c225f50aca395077d370993dc445e6
Amical Pay <amicalpay@gmail.com>
2026-09-14 01:55:03 -0400

    fix: remove unused FAQ translation

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,9 +1,6 @@
-import { useTranslation } from 'react-i18next'
 import { Card } from '@/components'
 
 function FAQ() {
-  const { t } = useTranslation()
-
   const faqs = [
     {
       question: 'Comment puis-je acheter des Diamonds?',
