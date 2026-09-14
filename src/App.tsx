b4658b220c3bd3bebf4797c849c3bb0aea12fc79
Amical Pay <amicalpay@gmail.com>
2026-09-14 09:53:21 -0400

    feat(header): mount header across the app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { AppProvider } from '@/contexts/AppContext'
 import { CartProvider } from '@/contexts/CartContext'
 import { BrowserRouter } from 'react-router-dom'
 import Router from '@/router'
+import Header from '@/components/Header'
 
 function App() {
   return (
     <BrowserRouter>
       <AppProvider>
         <CartProvider>
+          <Header />
           <Router />
         </CartProvider>
       </AppProvider>
