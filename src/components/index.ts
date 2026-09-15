f1e051c14732cfa49749fc350c84e6c28fa1b33c
Amical Pay <amicalpay@gmail.com>
2026-09-15 05:38:34 -0400

    Fix: restaurer le vrai contenu de components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,15 @@
-PLACEHOLDER
+export { Button } from './Button';
+export { Card } from './Card';
+export { Input } from './Input';
+export { Select } from './Select';
+export { Alert } from './Alert';
+export { LoadingSpinner } from './LoadingSpinner';
+export { default as Header } from './Header'
+export { MobileMenu } from './MobileMenu'
+export { UserMenu } from './UserMenu'
+export { Sidebar } from './Sidebar'
+export { FeatureStrip } from './FeatureStrip'
+export { CategoryGrid } from './CategoryGrid'
+export { WhyChooseUs } from './WhyChooseUs'
+export { Footer } from './Footer'
+export { HeroBanner } from './HeroBanner'
