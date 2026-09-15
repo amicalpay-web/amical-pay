ffbc49f0e05adbfab79083a3e22ccd937029e641
Amical Pay <amicalpay@gmail.com>
2026-09-15 05:36:11 -0400

    Refonte de la page d'accueil: sidebar catégories, bandeau avantages, grille catégories, bloc "pourquoi nous choisir", footer, header enrichi (HTG, support WhatsApp). Bannière Free Fire laissée en placeholder (asset officiel Garena non reproductible).

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,1 @@
-export { Button } from './Button';
-export { Card } from './Card';
-export { Input } from './Input';
-export { Select } from './Select';
-export { Alert } from './Alert';
-export { LoadingSpinner } from './LoadingSpinner';
-export { default as Header } from './Header'
-export { MobileMenu } from './MobileMenu'
-export { UserMenu } from './UserMenu'
+PLACEHOLDER
